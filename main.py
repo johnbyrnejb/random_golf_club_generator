d2c32fc7768c7cbd47cc6680cb140cf79ade7b17
John Byrne <johnbyrnejb@gmail.com>
2022-08-10 21:37:00 +0100

    Adding a new landing page. Moved the initial landing page to a new endpoint

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/club", response_class=HTMLResponse)
 async def return_homepage(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "club": select_random_club()})
-
