b41e458c19cdffa6180327fcbd129e3ca0f904dd
John Byrne <johnbyrnejb@gmail.com>
2022-08-12 21:32:51 +0100

    Added a new page just for some random nasa image with some explanation. Added a new home page that is not plugged into the others pages

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
 
 from app.random_club_generator import select_random_club
+from app.retrieve_random_space import return_nasa_info
 
 app = FastAPI()
 
@@ -19,5 +20,10 @@ async def root(request: Request):
 
 
 @app.get("/club", response_class=HTMLResponse)
-async def return_homepage(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "club": select_random_club()})
+async def return_random_club(request: Request):
+    return templates.TemplateResponse("random_club.html", {"request": request, "club": select_random_club()})
+
+
+@app.get("/space", response_class=HTMLResponse)
+async def return_random_space(request: Request):
+    return templates.TemplateResponse("random_nasa.html", {"request": request, "space": return_nasa_info()})
