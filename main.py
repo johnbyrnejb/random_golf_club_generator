95534ea5911c4f76da69b2ddb955c865d5db77eb
John Byrne <johnbyrnejb@gmail.com>
2022-08-12 21:37:48 +0100

    Commenting out other home pages

--- main.py
+++ main.py
@@ -14,14 +14,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/club", response_class=HTMLResponse)
-async def return_random_club(request: Request):
-    return templates.TemplateResponse("random_club.html", {"request": request, "club": select_random_club()})
+# @app.get("/")
+# async def root(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+#
+#
+# @app.get("/club", response_class=HTMLResponse)
+# async def return_random_club(request: Request):
+#     return templates.TemplateResponse("random_club.html", {"request": request, "club": select_random_club()})
 
 
 @app.get("/space", response_class=HTMLResponse)
